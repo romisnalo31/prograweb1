6eb174cace1c8d6a2ff804478828325d13470487
Rodrigo Manuel Navarro Lopez <romis_nalo@hotmail.com>
2014-06-27 16:20:50 -0500

    Primer pinshi

---
+++ practica1/guardar.php
@@ -0,0 +1,21 @@
+<?php
+	echo "Hola...";
+	
+	echo "Goles Equipo 1 Local:" . $_POST['equipo1_local'];
+	echo "<br/>";
+	echo "Goles Equipo 2 Visitante:" . $_POST['equipo2_visitante'];
+	
+	echo "<br/>";
+	echo "Goles Equipo 3 Local:" . $_POST['equipo3_local'];
+	echo "<br/>";
+	echo "Goles Equipo 4 Visitante:" . $_POST['equipo4_visitante'];
+	
+	echo "<br/>";
+	echo "Goles Equipo 3 Local:" . $_POST['equipo6_local'];
+	echo "<br/>";
+	echo "Goles Equipo 4 Visitante:" . $_POST['equipo8_local'];
+	echo "<br/>";
+	echo "Goles Equipo 3 Local:" . $_POST['equipo9_visitante'];
+	echo "<br/>";
+	echo "Goles Equipo 4 Visitante:" . $_POST['equipo7_visitante'];
+?>
